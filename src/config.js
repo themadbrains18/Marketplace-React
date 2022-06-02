bfb47292553abb3de10989984fc4a352887f893d
surindermdb <surinderkumar.mdb@gmail.com>
2022-06-02 16:25:23 +0530

    change varcel url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 // local config
 export const apiurl="http://localhost:3002/api/";
 export const imageUrl="http://localhost:3002/images/";
-export const hostName="http://localhost:3000"
+export const hostName="https://market-place-jade.vercel.app";
+// export const hostName="http://localhost:3000";
 
 // export const apiurl="https://peaceful-castle-24268.herokuapp.com/api/";
 // export const imageUrl="https://peaceful-castle-24268.herokuapp.com/images/";
